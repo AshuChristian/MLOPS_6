09815642ca694991a0b7ea8bc9b55410b150cf7d
AshuChristian <81282373+AshuChristian@users.noreply.github.com>
2023-10-02 21:31:19 +0530

    Update train.py
    Added report generation to metrics.txt

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import joblib
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 import pandas as pd 
 import numpy as np
 import os
@@ -22,5 +23,8 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 joblib.dump(model,os.path.join(model_dir,"linear_model.joblib"))
+
+with open("metrics.txt", 'w') as fw:
+  fw.write(f"MSE: {mean_squared_error(y_test, model.predict(X_test))}")
 
 print("Everthing done")
